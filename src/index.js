b7cd0de8dfb4e41488053f2f8f1bcd7ac1915db6
lufi479 <135998525+lufi479@users.noreply.github.com>
2023-06-28 16:00:39 -0700

    Add the necessary function calls for the web site

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import loadMain from "./main-page";
 import loadMenu from "./menu-page";
 import loadHeader from "./header";
 import loadFooter from "./footer";
-
-console.log(loadMain());
+import loadContact from "./contact-page";
 
 const content = document.getElementById("content");
 
@@ -11,3 +10,17 @@ const content = document.getElementById("content");
 content.appendChild(loadHeader());
 content.appendChild(loadMain());
 content.appendChild(loadFooter());
+
+
+document.querySelector(".nav-home").addEventListener("click", () =>{
+    content.replaceChild(loadMain(), content.childNodes[1]);
+});
+
+
+document.querySelector(".nav-menu").addEventListener("click", () =>{
+    content.replaceChild(loadMenu(), content.childNodes[1]);
+});
+
+document.querySelector(".nav-contact").addEventListener("click", () =>{
+    content.replaceChild(loadContact(), content.childNodes[1]);
+});
